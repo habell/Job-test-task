24eba1789f09a75b4f67d193fb983b178610c716
Habel <tamikys15@gmail.com>
2022-10-14 19:52:31 +0300

    spawn timer and spawn func

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -18,13 +18,14 @@ public class Game : MonoBehaviour
 
     private CubeMove _cubeMove;
 
-    private float _speed, _distance, _spawnTime;
+    private float _speed, _distance, _spawnTime, _spawnTimerCount;
 
     private void Awake()
     {
         ParseParameters();
         _cubePool = new CubePool(transform);
         _cubeMove = new CubeMove(_cubePool, _speed, _distance);
+        SpawnCube();
     }
 
     private void Update()
@@ -38,13 +39,23 @@ public class Game : MonoBehaviour
 
     private void FixedUpdate()
     {
+        _spawnTimerCount -= Time.fixedDeltaTime;
+        if(_spawnTimerCount <= 0) SpawnCube();
         _cubeMove.MoveCube();
+    }
+
+    private void SpawnCube()
+    {
+        _spawnTimerCount = _spawnTime;
+        var cube = _cubePool.GetCube();
+        _cubePool.SetActiveCube(cube);
     }
 
     private void ParseParameters()
     {
-        _speed = float.TryParse(_cubeSpeedInput.text, out _speed) ? _speed : 10f;
+        _speed = (float.TryParse(_cubeSpeedInput.text, out _speed) ? _speed : 10f) / 1000;
         _distance = float.TryParse(_cubeDistanceInput.text, out _distance) ? _distance : 10f;
         _spawnTime = float.TryParse(_cubeSpawnTimeInput.text, out _spawnTime) ? _spawnTime : 10f;
+        _spawnTimerCount = _spawnTime - _spawnTimerCount;
     }
 }
