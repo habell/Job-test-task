f44525a2f239ea473b212a768d324e8fb28de065
Habel <tamikys15@gmail.com>
2022-10-14 16:55:12 +0300

    new ctor

--- Assets/Scripts/CubeFactory.cs
+++ Assets/Scripts/CubeFactory.cs
@@ -1,17 +1,25 @@
-using System;
+using UnityEngine;
 using Object = UnityEngine.Object;
 
 public class CubeFactory : IFactory<Cube>
 {
     private readonly Cube _cube;
+
+    public CubeFactory()
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.SetActive(false);
+        cube.AddComponent<Cube>();
+        _cube = cube.GetComponent<Cube>();
+    }
+
     public CubeFactory(Cube cube)
     {
         _cube = cube;
     }
+
     public Cube Create()
     {
         return Object.Instantiate(_cube);
     }
-    
-    
 }
