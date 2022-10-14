cf5d7922cc34eafa7d81ac2b15b132e13a56d499
Habel <tamikys15@gmail.com>
2022-10-14 19:52:08 +0300

    add distance check and return cube to pool

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -5,33 +5,34 @@ using UnityEngine;
 public class CubeMove
 {
     private GameObject _cubeObject;
-    private List<Cube> _cubePool;
+    private CubePool _cubePool;
     private Transform _startPosition;
     private float _speed;
-    private float _distance;
+    private float _maxDistance;
 
-    public CubeMove(CubePool cubePool, float speed, float distance)
+    public CubeMove(CubePool cubePool, float speed, float maxDistance)
     {
-        SetParameters(speed, distance);
-        _cubePool = cubePool.GetPool();
-        _startPosition = cubePool.PoolParent.transform;
+        SetParameters(speed, maxDistance);
+        _cubePool = cubePool;
+        _startPosition = _cubePool.PoolParent.transform;
     }
 
-    public void SetParameters(float speed, float distance)
+    public void SetParameters(float speed, float maxDistance)
     {
         _speed = speed;
-        _distance = distance;
+        _maxDistance = maxDistance;
     }
 
     public void MoveCube()
     {
-        foreach (var cube in _cubePool)
+        foreach (var cube in _cubePool.GetPool())
         {
             _cubeObject = cube.gameObject;
             if (_cubeObject.activeSelf)
             {
-                _cubeObject.transform.Translate(Vector3.right / (1000f - _speed));
-                
+                _cubeObject.transform.Translate(Vector3.right * _speed);
+                var dist = Vector3.Distance(_startPosition.position, _cubeObject.transform.position);
+                if (dist >= _maxDistance) _cubePool.ReturnToPool(cube);
             }
         }
     }
