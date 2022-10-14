453f4b159e8db998fce4f8d87e996a6628251856
Habel <tamikys15@gmail.com>
2022-10-14 17:05:09 +0300

    inst pool and move script, add parse parameters and move func

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,5 +1,7 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Game : MonoBehaviour
 {
@@ -11,5 +13,33 @@ public class Game : MonoBehaviour
 
     [SerializeField]
     private TMP_InputField _cubeSpawnTimeInput;
-    
+
+    private CubePool _cubePool;
+
+    private CubeMove _cubeMove;
+
+    private float _speed, _distance, _spawnTime;
+
+    private void Awake()
+    {
+        _cubePool = new CubePool(transform);
+        _cubeMove = new CubeMove(_cubePool);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            _speed = float.TryParse(_cubeSpeedInput.text, out _speed) ? _speed : 10f;
+            _distance = float.TryParse(_cubeDistanceInput.text, out _distance) ? _distance : 10f;
+            _spawnTime = float.TryParse(_cubeSpawnTimeInput.text, out _spawnTime) ? _spawnTime : 10f;
+
+            _cubeMove.SetParameters(_speed, _distance);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _cubeMove.MoveCube();
+    }
 }
